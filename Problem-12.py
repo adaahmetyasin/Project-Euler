ff23c07ca4bb5c30d5479f3295fc9132f00921b2
adaahmetyasin <adaahmetyasinedu@gmail.com>
2022-01-11 14:46:43 +0300

    Problem

---
+++ Problem-12.py
@@ -0,0 +1,17 @@
+import math
+
+def bolen_sayisi(x):
+    bolen = 1
+    for i in range(2, int(math.sqrt(x) + 1)):
+        if x % i == 0:
+            bolen += 1
+    return bolen * 2
+
+n = 1
+
+while n > 0:
+    number = (n * (n + 1))/2
+    if bolen_sayisi(number) > 500:
+        print(number)
+        break
+    n += 1
